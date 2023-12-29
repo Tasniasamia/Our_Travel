e219094f10e11f18b681d3f42a30c96245f08f32
tasniaarhamarham <tasniaarhamarham@gmail.com>
2023-12-29 14:56:29 +0600

    done

--- src/components/News_List/List_details.jsx
+++ src/components/News_List/List_details.jsx
@@ -1,7 +1,97 @@
 import img1 from "../../assets/img/blog/news-list-1.png";
 import img2 from "../../assets/img/blog/news-list-round.png";
 import img3 from "../../assets/img/blog/news-list-2.png";
+import img4 from "../../assets/img/blog/news-list-round.png" ;
+import img5 from "../../assets/img/blog/news-list-3.png";
+import img6 from "../../assets/img/blog/news-list-round.png";
+import img7 from "../../assets/img/blog/news-list-4.png";
+import img8 from "../../assets/img/blog/news-list-round.png";
+import img9 from "../../assets/img/icon/serach-icon.png" ;
+import { Link } from "react-router-dom";
 const List_details = () => {
+const category=[
+{  name:"Travel agent"}  ,
+{  name:"Honeymoon"}  ,
+{  name:"Flights"}  ,
+{  name:"Adventure"}  ,
+{  name:"Hotels"}  ,
+{  name:"Apartments"}  ,
+{  name:"Flight service"}  ,
+]
+const popular_tags=[
+    {
+        tag:"Party"
+    },
+    {
+        tag:"Conference"
+    },
+    {
+        tag:"Cakes"
+    },
+    {
+        tag:"Decorations"
+    },
+    {
+        tag:"Wedding party"
+    },
+    {
+        tag:"Dance"
+    },
+    {
+        tag:"Finance"
+    },
+    {
+        tag:"Warehouse"
+    },
+    {
+        tag:"Business party"
+    },
+    
+]
+
+const news=[
+    {
+        img:img1,
+        img2:img2,
+        name:"Melisa campbell",
+        date:"26 Oct 2021",
+        read:"8 min read",
+        title:"Veniam ex tempor qui ad amet mollit cillum aliqua aliqua. Fugiat tempor eu magna",
+        subtitle:"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its fru layout. The point of using Lorem Ipsum is that it has a more."
+
+    },
+    {
+        img:img3,
+        img2:img4,
+        name:"Melisa campbell",
+        date:"26 Oct 2021",
+        read:"8 min read",
+        title:"Veniam ex tempor qui ad amet mollit cillum aliqua aliqua. Fugiat tempor eu magna",
+        subtitle:"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its fru layout. The point of using Lorem Ipsum is that it has a more."
+
+    },
+    {
+        img:img5,
+        img2:img6,
+        name:"Melisa campbell",
+        date:"26 Oct 2021",
+        read:"8 min read",
+        title:"Veniam ex tempor qui ad amet mollit cillum aliqua aliqua. Fugiat tempor eu magna",
+        subtitle:"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its fru layout. The point of using Lorem Ipsum is that it has a more."
+
+    },
+    {
+        img:img7,
+        img2:img8,
+        name:"Melisa campbell",
+        date:"26 Oct 2021",
+        read:"8 min read",
+        title:"Veniam ex tempor qui ad amet mollit cillum aliqua aliqua. Fugiat tempor eu magna",
+        subtitle:"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its fru layout. The point of using Lorem Ipsum is that it has a more."
+
+    },
+]
+
     return (
         <div>
              <section id="news-list" className="section-padding">
@@ -17,104 +107,34 @@ const List_details = () => {
             <div className="news-list-area-wrapper">
                 <div className="row">
                     <div className="col-lg-8">
-                        <div className="news-list-card">
-                            <div className="news-list-img">
-                                <a href="news-details.html"><img src={img1} alt="img"/></a>
-                            </div>
-                            <div className="news-list-text">
-                                <div className="news-list-info">
-                                    <div className="news-list-author-info">
-                                        <img src={img2} alt="img"/>
-                                        <h6>Melisa campbell</h6>
+                        {
+                            news.map((item,index)=>{
+                                return (
+                                    <div className="news-list-card" key={index}>
+                                    <div className="news-list-img">
+                                        <a href="news-details.html"><img src={item.img} alt="img"/></a>
                                     </div>
-                                    <div className="news-list-date">
-                                        <ul>
-                                            <li>26 Oct 2021</li>
-                                            <li>8 min read</li>
-                                        </ul>
-                                    </div>
-                                </div>
-                                <h2><a href="news-details.html">Veniam ex tempor qui ad amet mollit cillum aliqua
-                                        aliqua. Fugiat tempor eu magna</a></h2>
-                                <p>It is a long established fact that a reader will be distracted by the readable
-                                    content of a page when looking at its fru layout. The point of using Lorem Ipsum is
-                                    that it has a more. <span><a href="news-details.html">Read more...</a></span></p>
-                            </div>
-                        </div>
-                        <div className="news-list-card">
-                            <div className="news-list-img">
-                                <a href="news-details.html"><img src={img3} alt="img"/></a>
-                            </div>
-                            <div className="news-list-text">
-                                <div className="news-list-info">
-                                    <div className="news-list-author-info">
-                                        <img src="assets/img/blog/news-list-round.png" alt="img">
-                                        <h6>Melisa campbell</h6>
-                                    </div>
-                                    <div className="news-list-date">
-                                        <ul>
-                                            <li>26 Oct 2021</li>
-                                            <li>8 min read</li>
-                                        </ul>
+                                    <div className="news-list-text">
+                                        <div className="news-list-info">
+                                            <div className="news-list-author-info">
+                                                <img src={item.img2} alt="img"/>
+                                                <h6>{item.name}</h6>
+                                            </div>
+                                            <div className="news-list-date">
+                                                <ul>
+                                                    <li>{item.date}</li>
+                                                    <li>{item.read}</li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                        <h2><Link to="/news_details">{item.title}</Link></h2>
+                                        <p>{item.subtitle}<span><Link to="/news_details">Read more...</Link></span></p>
                                     </div>
                                 </div>
-                                <h2><a href="news-details.html">Veniam ex tempor qui ad amet mollit cillum aliqua
-                                        aliqua. Fugiat tempor eu magna</a></h2>
-                                <p>It is a long established fact that a reader will be distracted by the readable
-                                    content of a page when looking at its fru layout. The point of using Lorem Ipsum is
-                                    that it has a more. <span><a href="news-details.html">Read more...</a></span></p>
-                            </div>
-                        </div>
-                        <div className="news-list-card">
-                            <div className="news-list-img">
-                                <a href="news-details.html"><img src="assets/img/blog/news-list-3.png" alt="img"></a>
-                            </div>
-                            <div className="news-list-text">
-                                <div className="news-list-info">
-                                    <div className="news-list-author-info">
-                                        <img src="assets/img/blog/news-list-round.png" alt="img">
-                                        <h6>Melisa campbell</h6>
-                                    </div>
-                                    <div className="news-list-date">
-                                        <ul>
-                                            <li>26 Oct 2021</li>
-                                            <li>8 min read</li>
-                                        </ul>
-                                    </div>
-                                </div>
-                                <h2><a href="news-details.html">Veniam ex tempor qui ad amet mollit cillum aliqua
-                                        aliqua. Fugiat tempor eu magna</a>
-                                </h2>
-                                <p>It is a long established fact that a reader will be distracted by the readable
-                                    content of a page when looking at its fru layout. The point of using Lorem Ipsum is
-                                    that it has a more. <span><a href="news-details.html">Read more...</a></span></p>
-                            </div>
-                        </div>
-                        <div className="news-list-card">
-                            <div className="news-list-img">
-                                <a href="news-details.html"><img src="assets/img/blog/news-list-4.png" alt="img"></a>
-                            </div>
-                            <div className="news-list-text">
-                                <div className="news-list-info">
-                                    <div className="news-list-author-info">
-                                        <img src="assets/img/blog/news-list-round.png" alt="img">
-                                        <h6>Melisa campbell</h6>
-                                    </div>
-                                    <div className="news-list-date">
-                                        <ul>
-                                            <li>26 Oct 2021</li>
-                                            <li>8 min read</li>
-                                        </ul>
-                                    </div>
-                                </div>
-                                <h2><a href="news-details.html">Veniam ex tempor qui ad amet mollit cillum aliqua
-                                        aliqua. Fugiat tempor eu magna</a>
-                                </h2>
-                                <p>It is a long established fact that a reader will be distracted by the readable
-                                    content of a page when looking at its fru layout. The point of using Lorem Ipsum is
-                                    that it has a more. <span><a href="news-details.html">Read more...</a></span></p>
-                            </div>
-                        </div>
+                                )
+                            })
+                        }
+                     
                         <div className="row">
                             <div className="col-lg-6 offset-lg-3">
                                 <div className="count-page">
@@ -147,21 +167,23 @@ const List_details = () => {
                         <div className="news-list-right">
                             <div className="news-list-search-field">
                                 <div className="news-list-input">
-                                    <input type="text" placeholder="Search here">
-                                    <a href="#!"><img src="assets/img/icon/serach-icon.png" alt="icon"></a>
+                                    <input type="text" placeholder="Search here"/>
+                                    <a href="#!"><img src={img9} alt="icon"/></a>
                                 </div>
                             </div>
                             <div className="news-list-category-wrapper">
                                 <h3><span className="section-after">Category</span></h3>
                                 <div className="category-list">
                                     <ul>
-                                        <li><a href="#!">Travel agent </a></li>
-                                        <li><a href="#!">Honeymoon</a></li>
-                                        <li><a href="#!">Flights</a></li>
-                                        <li><a href="#!">Adventure</a></li>
-                                        <li><a href="#!">Hotels</a></li>
-                                        <li><a href="#!">Apartments</a></li>
-                                        <li><a href="#!">Flight service</a></li>
+                                        {
+                                            category.map((item,index)=>{
+                                                return (
+                                                    <li key={index}><a href="#!">{item.name} </a></li>
+
+                                                )
+                                            })
+                                        }
+                                      
                                     </ul>
                                 </div>
                             </div>
@@ -169,15 +191,16 @@ const List_details = () => {
                                 <h3><span className="section-after">Popular tags</span></h3>
                                 <div className="popular-tags">
                                     <ul>
-                                        <li><a href="news.html">Party</a></li>
-                                        <li><a href="news.html">Conference</a></li>
-                                        <li><a href="news.html">Cakes</a></li>
-                                        <li><a href="news.html">Decorations</a></li>
-                                        <li><a href="news.html">Wedding party</a></li>
-                                        <li><a href="news.html">Dance</a></li>
-                                        <li><a href="news.html">Finance</a></li>
-                                        <li><a href="news.html">Warehouse </a></li>
-                                        <li><a href="news.html">Business party</a></li>
+
+                                        {
+                                            popular_tags.map((item,index)=>{
+                                                return (
+                                                    <li key={index}><Link to="/news">{item.tag}</Link></li>
+
+                                                )
+                                            })
+                                        }
+                                     
                                     </ul>
                                 </div>
                             </div>
--- src/components/News_List/News_L.jsx
+++ src/components/News_List/News_L.jsx
@@ -1,9 +1,11 @@
 import Banner from "./Banner";
+import List_details from "./List_details";
 
 const News_L = () => {
     return (
         <div>
             <Banner/>
+            <List_details/>
             
         </div>
     );
